ba3b31f4b2238130af25393cda5327c57a968e61
tohdarryl <thgdarryl@gmail.com>
2022-12-03 14:50:39 +0800

    day01workshop

--- src/cart/Main.java
+++ src/cart/Main.java
@@ -9,12 +9,12 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        
+        System.out.println("Welcome to your shopping cart");
         List<String> cart = new LinkedList<>();
         Console cons = System.console();
         Boolean stop = false;
 
-        while (!stop) { 
+        while (!stop) {//does not stop until user prompts exit
             // list 
             // add <item>
             // delete <num>
@@ -28,7 +28,8 @@ public class Main {
             // list +> [list]
             String[] terms = line.split(" ");
 
-            System.out.printf("Command $s\n", terms[0]);
+            
+            System.out.printf("Command %s\n", terms[0]);
 
             switch (terms[0]) {
                 case "list":
@@ -36,17 +37,34 @@ public class Main {
                         System.out.println("Your cart is empty");
                     } else {
                     System.out.println("Contents of your cart");
-                    for (Integer idx = 0; idx < cart.size(); idx++)
+                    for (int idx = 0; idx < cart.size(); idx++)
                     System.out.printf("%d. %s\n", idx + 1, cart.get(idx));
                     }
-                break;
+                break; //should be after if statement and for loop
 
                 case "add":
-                cart.add(terms[1]);
-                System.out.printf("Added %s to cart\n", terms [1]);
+                    if (cart.contains(terms[1])){
+                        System.out.printf("You have %s in your cart\n", terms[1]);
+                    }
+                    else {
+                    for (int i = 1; i < terms.length; i++){
+                    cart.add(terms[i]);
+                    System.out.printf("%s added to cart\n", terms [i]);
+                    }}
                 break;
 
                 case "delete":
+                    //create new variable to make input and index consistent
+                    //also converting String into int, remember to use brackets around ((var) + 1)
+                    int ind = (Integer.parseInt(terms[1]) - 1);
+                    if(Integer.parseInt(terms[1]) > cart.size()){
+                    System.out.println("Incorrect item index");}
+                    else {
+                    //print item removed associated with number
+                    //to remove item by number
+                    System.out.printf("%s removed from cart\n", cart.get(ind));
+                    cart.remove(ind);}
+                    
                 break;
 
                 case "exit":
@@ -55,7 +73,7 @@ public class Main {
 
 
                 default:
-                System.out.printf("Unknown command $s\n", terms[0]);
+                System.out.printf("Unknown command %s\n", terms[0]);
             }
         }
 
